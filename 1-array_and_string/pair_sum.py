948522548440fa19c7c3e2acce353055c7fc6c0c
Quyen Hoang <qduhoang@gmail.com>
2022-12-15 02:12:11 -0800

    pair_sum brute force complete

--- 1-array_and_string/pair_sum.py
+++ 1-array_and_string/pair_sum.py
@@ -6,3 +6,11 @@ def pair_sum(numbers, target_sum):
       return (hash_map[y], index)
     else:
       hash_map[value] = index
+
+
+# brute force
+def pair_sum(numbers, target_sum):
+  for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+      if numbers[i] + numbers[j] == target_sum:
+        return (i, j)
