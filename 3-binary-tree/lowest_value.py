9bc1fe262a4eaec61fb6af0ba66fafa147266d34
Quyen Hoang <qduhoang@gmail.com>
2022-12-27 16:28:34 -0800

    bottom right value complete

---
+++ 3-binary-tree/lowest_value.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def bottom_right_value(root):
+  if root is None:
+    return None
+
+  queue = deque([ root ])
+  while queue:
+    current = queue.popleft()
+
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+
+  return current.val
