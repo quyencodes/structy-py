ccbef8965f2d8547ddc6454d6627fcfdf4f115a2
Quyen Hoang <qduhoang@gmail.com>
2023-01-01 00:33:19 -0800

    dfs attempt 3

---
+++ review2/3-binary_tree/dfs.py
@@ -0,0 +1,8 @@
+def depth_first_values(root):
+  if root is None:
+    return []
+
+  left_values = depth_first_values(root.left)
+  right_values = depth_first_values(root.right)
+
+  return [root.val, *left_values, *right_values]
