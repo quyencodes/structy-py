66cbd971653028d9908fa602c967e6973e0b5d27
Quyen Hoang <qduhoang@gmail.com>
2023-01-03 07:02:07 -0800

    warmup complete

---
+++ 4-graphs/warmup.py
@@ -0,0 +1,39 @@
+# def depth_first_print(graph, start):
+#   stack = [ start ]
+#   while len(stack) > 0:
+#     current = stack[-1]
+#     print(current)
+#     stack.pop()
+
+#     for neighbor in graph[current]:
+#       stack.append(neighbor)
+
+def depth_first_print(graph, current):
+  print(current)
+  for neighbor in graph[current]:
+    depth_first_print(graph, neighbor)
+
+from collections import deque
+def breadth_first_print(graph, start):
+  queue = deque([ start ])
+  while queue:
+    current = queue[0]
+    print(current)
+    queue.popleft()
+    for neighbor in graph[current]:
+      queue.append(neighbor)
+
+
+
+graph = {
+  "a": ["b", "c"],
+  "b": ["d"],
+  "c": ["e"],
+  "d": ["f"],
+  "e": [],
+  "f": []
+}
+
+
+
+depth_first_print(graph, "a")
