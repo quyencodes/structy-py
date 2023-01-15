1b5632cf48c9f32a69c17f113e62934f8637495a
Quyen Hoang <qduhoang@gmail.com>
2023-01-14 22:10:34 -0800

    more intersection solutions

--- 9-dailies/0-array-and-strings/intersection.py
+++ 9-dailies/0-array-and-strings/intersection.py
@@ -1,2 +1,14 @@
 def intersection(a, b):
   return list(set(a).intersection(b))
+
+def intersection(a, b):
+  set_A = set(a)
+  return [item for item in b if item in set_A]
+
+def intersection(a, b):
+  result = []
+  for num in b:
+    if num in a:
+      result.append(num)
+
+  return result
