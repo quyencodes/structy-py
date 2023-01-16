23092e063299f5015168d91b34e61d1f85f33f72
Quyen Hoang <qduhoang@gmail.com>
2023-01-15 21:55:06 -0800

    intersection redo

--- 9-dailies/0-array-and-strings/intersection.py
+++ 9-dailies/0-array-and-strings/intersection.py
@@ -2,8 +2,8 @@ def intersection(a, b):
   return list(set(a).intersection(b))
 
 def intersection(a, b):
-  set_A = set(a)
-  return [item for item in b if item in set_A]
+  set_a = set(a)
+  return [item for item in b if item in set_a]
 
 def intersection(a, b):
   result = []
