bf8b2a7b2b261b4679bef34943bcd06514789e77
Quyen Hoang <qduhoang@gmail.com>
2023-01-19 12:47:47 -0800

    five sort complete

---
+++ 101-practice-1-19-22/1-array_and_string/five_sort.py
@@ -0,0 +1,16 @@
+def five_sort(nums):
+  i = 0
+  j = len(nums) - 1
+
+  while i <= j:
+    if nums[j] == 5:
+      j -= 1
+    else:
+      if nums[i] == 5:
+        nums[i], nums[j] = nums[j], nums[i]
+      else:
+        i += 1
+  return nums
+
+#.       i  j
+# [4, 1, 1, 1, 5, 5, 5]
