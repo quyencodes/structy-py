e692b93569a9d543aaffe3018b3e662861ae87b6
Quyen Hoang <qduhoang@gmail.com>
2023-01-19 01:30:03 -0800

    is prime and max value

---
+++ 101-practice-1-19-22/1-array_and_string/max_value.py
@@ -0,0 +1,7 @@
+# input - array of numbers
+# output - max value in the array of numbers
+# constraints - none
+# edge cases - non int in the array, non array
+
+def max_value(nums):
+  return max(nums)
