2e3fe78a1748a800af6043279e43f32f40fbf3e5
Quyen Hoang <qduhoang@gmail.com>
2023-01-23 18:04:18 -0800

    reverse linked list review

--- review/2-linked-lists/get_node_value.py
+++ review/2-linked-lists/get_node_value.py
@@ -17,3 +17,14 @@ def get_node_value(head, index):
     current = current.next
 
   return None
+
+# time - o(n)
+# space - o(n)
+def get_node_value(head, index):
+  if head is None:
+    return None
+
+  if index == 0:
+    return head.val
+
+  return get_node_value(head.next, index - 1)
