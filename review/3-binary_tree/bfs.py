819c574e8d3a6ef50a7eeb16510dd181d60fcdc6
Quyen Hoang <qduhoang@gmail.com>
2023-01-25 19:20:32 -0800

    bfs complete

---
+++ review/3-binary_tree/bfs.py
@@ -0,0 +1,13 @@
+from collections import deque
+def breadth_first_values(root):
+  if not root: return []
+  values = []
+  queue = deque([root])
+  while queue:
+    current = queue.popleft()
+    values.append(current.val)
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+  return values
--- review/3-binary_tree/dfs.py
+++ review/3-binary_tree/dfs.py
@@ -11,7 +11,7 @@ def depth_first_values(root):
   left_values = depth_first_values(root.left)
   right_values = depth_first_values(root.right)
 
-  return [root.val, *left_values, *right_values]
+  return [ root.val ] + left_values + right_values
 
 def depth_first_values(root):
   if root is None: return []
