878dae67b6d08cb3fd6da7cf8e002d5fae69ec7d
Quyen Hoang <qduhoang@gmail.com>
2023-01-31 00:40:02 -0800

    longest path complete

---
+++ review/4-graphs/longest_path.py
@@ -0,0 +1,37 @@
+def longest_path(graph):
+  distances = {}
+  # store terminal nodes
+  for node in graph:
+    if len(graph[node]) == 0:
+      distances[node] = 0
+
+  print(distances)
+
+  for node in graph:
+    if node not in distances:
+      explore_nodes(graph, node, distances)
+
+  return max(distances.values())
+
+def explore_nodes(graph, current, distances):
+  # if its in there
+  if current in distances:
+    return distances[current]
+
+  largest = 0
+  for neighbor in graph[current]:
+    attempt = explore_nodes(graph, neighbor, distances)
+    if attempt > largest:
+      largest = attempt
+
+  distances[current] = 1 + largest
+  return distances[current]
+
+example = {
+  'a': ['c', 'b'],
+  'b': ['c'],
+  'c': []
+}
+
+print(longest_path(example))
+
