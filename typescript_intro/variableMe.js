3c40066b3dae5189ba05e4edd9c896395046d9ba
Quyen Hoang <qduhoang@gmail.com>
2023-02-11 05:44:32 -0800

    typescript and testing __name__ special variable

---
+++ typescript_intro/variableMe.js
@@ -0,0 +1,2 @@
+var greetings = 'Hello Quyen';
+console.log(greetings);
---
+++ typescript_intro/variableMe.ts
@@ -0,0 +1,6 @@
+let greetings: string = 'Hello Quyen';
+let mynum: number = 6;
+greetings.toLowerCase();
+console.log(greetings);
+
+export {};
