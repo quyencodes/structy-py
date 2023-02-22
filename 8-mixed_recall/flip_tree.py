03434dec8afd5662f3f7cec624d60df0fefe3d4d
Quyen Hoang <qduhoang@gmail.com>
2023-02-22 02:11:23 -0800

    flip tree and left nodes complete

--- 8-mixed_recall/flip_tree.py
+++ 8-mixed_recall/flip_tree.py
@@ -1,1 +1,26 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+from collections import deque
 def flip_tree(root):
+  if root is None: return None
+  queue = deque([root])
+  while queue:
+    current = queue.popleft()
+    if current.left:
+      queue.append(current.left)
+    if current.right:
+      queue.append(current.right)
+
+    current.left, current.right = current.right, current.left
+
+  return root
+
+#   if root is None:
+#     return None
+
+#   root.left, root.right = flip_tree(root.right), flip_tree(root.left)
+#   return root
--- 8-mixed_recall/left_nodes.py
+++ 8-mixed_recall/left_nodes.py
@@ -3,7 +3,60 @@ class Node:
     self.val = val
     self.left = None
     self.right = None
+
 import collections
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def lefty_nodes(root):
+  lefty = []
+  _lefty_nodes(root, 0, lefty)
+  return lefty
+
+
+def _lefty_nodes(root, level, lefty):
+  if root is None:
+    return
+
+  if len(lefty) == level:
+    lefty.append(root.val)
+
+  _lefty_nodes(root.left, level+1, lefty)
+  _lefty_nodes(root.right, level+1, lefty)
+
+"""
+bfs levels traversal
+[[u], [t, s], [r], [q, p]]
+popleft() each sublist
+"""
+
+u = Node('u')
+t = Node('t')
+s = Node('s')
+r = Node('r')
+q = Node('q')
+p = Node('p')
+
+u.left = t
+u.right = s
+s.right = r
+r.left = q
+r.right = p
+
+#     u
+#  /    \
+# t      s
+#         \
+#         r
+#        / \
+#        q  p
+
+lefty_nodes(u) # [ 'u', 't', 'r', 'q' ]
+
+
 def lefty_nodes(root):
   levels = levels_traversal(root)
   levels = collections.deque(levels)
