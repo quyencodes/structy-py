047fa4c42bfc80ea6fb8341b644cb99c90d6b1e4
Quyen Hoang <qduhoang@gmail.com>
2023-02-25 17:42:08 -0800

    post order traversal

---
+++ 8-mixed_recall/post_order_traversal.py
@@ -0,0 +1,17 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def post_order(root):
+  values = []
+  post_order_traversal(root, values)
+  return values
+
+def post_order_traversal(root, values):
+  if root is None:
+    return
+  post_order_traversal(root.left, values)
+  post_order_traversal(root.right, values)
+  values.append(root.val)
