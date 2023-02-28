aa4b3e5932bd30cead05bb0d69b1d96ef31a43d5
Quyen Hoang <qduhoang@gmail.com>
2023-02-27 16:02:18 -0800

    topological order redo

--- 8-mixed_recall/topological_order.py
+++ 8-mixed_recall/topological_order.py
@@ -19,3 +19,36 @@ def topological_order(graph):
         ready.append(child)
 
   return order
+
+from collections import deque
+def topological_order(graph):
+  num_parents = {}
+  for node in graph:
+    num_parents[node] = 0
+
+  for node in graph:
+    for child in graph[node]:
+      num_parents[child] += 1
+
+  print(num_parents)
+  ready = deque([ node for node in graph if num_parents[node] == 0 ])
+  order = []
+
+  while ready:
+    node = ready.popleft()
+    order.append(node)
+    for child in graph[node]:
+      num_parents[child] -= 1
+      if num_parents[child] == 0:
+        ready.append(child)
+
+  return order
+
+topological_order({
+  "a": ["f"],
+  "b": ["d"],
+  "c": ["a", "f"],
+  "d": ["e"],
+  "e": [],
+  "f": ["b", "e"],
+})
