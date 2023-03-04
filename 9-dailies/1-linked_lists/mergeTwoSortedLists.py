4f6b313c9fa83f3efacb625d7fcc0d54e1f4b8ec
Quyen Hoang <qduhoang@gmail.com>
2023-03-03 22:55:41 -0800

    merge two linked lists attempt

--- 9-dailies/1-linked_lists/mergeTwoSortedLists.py
+++ 9-dailies/1-linked_lists/mergeTwoSortedLists.py
@@ -29,21 +29,21 @@ current_2 = 2
 """
 
 
-  # def mergeTwoSortedLists(self, head_1, head_2):
-  #   if head_1 is None and head_2 is None:
-  #     return None
+def mergeTwoSortedLists(self, head_1, head_2):
+  if head_1 is None and head_2 is None:
+    return None
 
-  #   if head_1 is None:
-  #     return head_2
+  if head_1 is None:
+    return head_2
 
-  #   if head_2 is None:
-  #     return head_1
+  if head_2 is None:
+    return head_1
 
-  #   if head_1.val <= head_2.val:
-  #     saved_next = head_1.next
-  #     head_1.next = self.mergeTwoSortedLists(saved_next, head_2)
-  #     return head_1
-  #   else:
-  #     saved_next = head_2.next
-  #     head_2.next = self.mergeTwoSortedLists(head_1, saved_next)
-  #     return head_2
+  if head_1.val <= head_2.val:
+    saved_next = head_1.next
+    head_1.next = self.mergeTwoSortedLists(saved_next, head_2)
+    return head_1
+  else:
+    saved_next = head_2.next
+    head_2.next = self.mergeTwoSortedLists(head_1, saved_next)
+    return head_2
