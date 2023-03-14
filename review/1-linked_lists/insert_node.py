d7a98f562fe9d122edad815011876bd94d5c3b89
Quyen Hoang <qduhoang@gmail.com>
2023-03-14 00:59:34 -0700

    insert node iterative

--- review/1-linked_lists/insert_node.py
+++ review/1-linked_lists/insert_node.py
@@ -6,3 +6,25 @@ def insert_node(head, value, index):
 
   head.next = insert_node(head.next, value, index-1)
   return head
+
+def insert_node(head, value, index):
+  dummy_head = Node(None)
+  tail = dummy_head
+  current = head
+
+  while current is not None:
+    if index == 0:
+      new_node = Node(value)
+      new_node.next = current
+      tail.next = new_node
+      return dummy_head.next
+
+    index -= 1
+    tail.next = current
+    tail = tail.next
+    current = current.next
+
+  if index == 0:
+    tail.next = Node(value)
+
+  return dummy_head.next
