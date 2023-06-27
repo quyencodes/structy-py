a28854daa986f2a2f85aaf2cfa3fc4aa5f36db25
Quyen Hoang <qduhoang@gmail.com>
2023-06-27 15:04:50 -0700

    linkedList palindrome complete

--- 8-mixed_recall/linked_palindrome.py
+++ 8-mixed_recall/linked_palindrome.py
@@ -1,32 +1,32 @@
+class Node(object):
+  def __init__(self, val, next=None):
+    self.val = val
+    self.next = next
+
 def linked_palindrome(head):
-  # reverse the linked list
+  curr = head
+  values = []
+
+  while curr is not None:
+    values.append(curr.val)
+    curr = curr.next
+
+  return curr == curr[::-1]
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def linked_palindrome(head):
   rev_head = reverse_linkedlist(head)
-  # check each node one by one
-  return check_palindrome(head, rev_head)
+  while head is not None and rev_head is not None:
+    if head.val != rev_head.val:
+      return False
+    head = head.next
+    rev_head = rev_head.next
 
-def check_palindrome(head_1, head_2):
-  if head_1 is None and head_2 is None:
-    return True
-
-  if head_1.val != head_2.val:
-    return False
-
-  return check_palindrome(head_1.next, head_2.next)
-
-def reverse_linkedlist(head):
-  prev = None
-  current = head
-  while current:
-    saved_next = current.next
-    current.next = prev
-    prev = current
-    current = saved_next
-
-  return prev
-
-# None <- a -> b -> c -> None
-#         p    c
-# sn = b
+  return True
 
 def reverse_linkedlist(head):
   if head is None:
@@ -36,6 +36,6 @@ def reverse_linkedlist(head):
     return head
 
   new_head = reverse_linkedlist(head.next)
-  new_head.next.next = head
   head.next = None
+  new_head.next = head
   return new_head
