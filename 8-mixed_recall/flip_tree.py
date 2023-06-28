0a847504c7b274faa800a3ec5fa15d3cef54a6c4
Quyen Hoang <qduhoang@gmail.com>
2023-06-28 15:14:04 -0700

    flipTree complete

--- 8-mixed_recall/flip_tree.py
+++ 8-mixed_recall/flip_tree.py
@@ -1,26 +1,10 @@
-# class Node:
-#   def __init__(self, val):
-#     self.val = val
-#     self.left = None
-#     self.right = None
-
-from collections import deque
 def flip_tree(root):
-  if root is None: return None
-  queue = deque([root])
-  while queue:
-    current = queue.popleft()
-    if current.left:
-      queue.append(current.left)
-    if current.right:
-      queue.append(current.right)
+  if root is None:
+    return None
 
-    current.left, current.right = current.right, current.left
+  left_side = flip_tree(root.left)
+  right_side = flip_tree(root.right)
 
+  root.left = right_side
+  root.right = left_side
   return root
-
-#   if root is None:
-#     return None
-
-#   root.left, root.right = flip_tree(root.right), flip_tree(root.left)
-#   return root
