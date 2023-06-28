86da879f96f03f413ebb9a5afaeb72f054715f2e
Quyen Hoang <qduhoang@gmail.com>
2023-06-28 15:36:45 -0700

    leftyNodes complete

--- 8-mixed_recall/left_nodes.py
+++ 8-mixed_recall/left_nodes.py
@@ -1,115 +1,42 @@
-class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
-
-import collections
-class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
-
 def lefty_nodes(root):
-  lefty = []
-  _lefty_nodes(root, 0, lefty)
-  return lefty
+  res = []
+  _lefty_nodes(root, res, 0)
+  return res
 
-
-def _lefty_nodes(root, level, lefty):
+def _lefty_nodes(root, nodes, level):
   if root is None:
     return
 
-  if len(lefty) == level:
-    lefty.append(root.val)
+  if len(nodes) == level:
+    nodes.append(root.val)
 
-  _lefty_nodes(root.left, level+1, lefty)
-  _lefty_nodes(root.right, level+1, lefty)
+  _lefty_nodes(root.left, nodes, level+1)
+  _lefty_nodes(root.right, nodes, level+1)
 
-"""
-bfs levels traversal
-[[u], [t, s], [r], [q, p]]
-popleft() each sublist
-"""
+  return
 
-u = Node('u')
-t = Node('t')
-s = Node('s')
-r = Node('r')
-q = Node('q')
-p = Node('p')
-
-u.left = t
-u.right = s
-s.right = r
-r.left = q
-r.right = p
-
-#     u
-#  /    \
-# t      s
-#         \
-#         r
-#        / \
-#        q  p
-
-lefty_nodes(u) # [ 'u', 't', 'r', 'q' ]
-
-
+from collections import deque
 def lefty_nodes(root):
-  levels = levels_traversal(root)
-  levels = collections.deque(levels)
-  res = []
-  for level in levels:
-    res.append(level.pop(0))
-  return res
-
-
-def levels_traversal(root):
-  if root is None: return []
   levels = []
-  queue = collections.deque([(root, 0)])
-  while queue:
-    current, lvl = queue.popleft()
+  level_num = 0
+  if root is None: return []
+  queue = deque([(root, level_num)])
 
-    if len(levels) == lvl:
+  while queue:
+    current, level = queue.popleft()
+
+    if len(levels) == level:
       levels.append([current.val])
     else:
-      levels[lvl].append(current.val)
+      levels[level].append(current.val)
 
-    if current.left:
-      queue.append((current.left, lvl+1))
-    if current.right:
-      queue.append((current.right, lvl+1))
+    if current.left is not None:
+      queue.append((current.left, level+1))
+    if current.right is not None:
+      queue.append((current.right, level+1))
 
-  return levels
+  res = []
+  for level in levels:
+    res.append(level[0])
 
-"""
-bfs levels traversal
-[[u], [t, s], [r], [q, p]]
-popleft() each sublist
-"""
-
-u = Node('u')
-t = Node('t')
-s = Node('s')
-r = Node('r')
-q = Node('q')
-p = Node('p')
-
-u.left = t
-u.right = s
-s.right = r
-r.left = q
-r.right = p
-
-#     u
-#  /    \
-# t      s
-#         \
-#         r
-#        / \
-#        q  p
-
-lefty_nodes(u) # [ 'u', 't', 'r', 'q' ]
+  return res
