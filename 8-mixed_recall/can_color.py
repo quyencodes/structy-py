1bf610bf16d117de266c3ae4ac311d9be1fb1df9
Quyen Hoang <qduhoang@gmail.com>
2023-06-28 16:26:33 -0700

    canColor complete

--- 8-mixed_recall/can_color.py
+++ 8-mixed_recall/can_color.py
@@ -1,23 +1,51 @@
 def can_color(graph):
-  coloring = {}
+  colors = {} # node - color
+  color_code = True # even - red, odd - blue
   for node in graph:
-    if node not in coloring and not validate(graph, node, coloring, False):
-      return False
-  return True
-
-def validate(graph, node, coloring, current_color):
-  if node in coloring:
-    return current_color == coloring[node]
-
-  coloring[node] = current_color
-
-  for neighbor in graph[node]:
-    if validate(graph, neighbor, coloring, not current_color) == False:
+    if node not in colors and _can_color(graph, node, colors, not color_code) == False:
       return False
 
   return True
 
-"""
-red = x
-blue = y
-"""
+def _can_color(graph, current, colors, code):
+  if current in colors and colors[current] != code:
+    return False
+  elif current in colors and colors[current] == code:
+    return True
+
+  colors[current] = code
+
+  for neighbor in graph[current]:
+    if _can_color(graph, neighbor, colors, not code) == False:
+      return False
+
+  return True
+
+
+def detect_cycle(graph):
+  visited = set()
+  visiting = set()
+  for node in graph:
+    if has_cycle(graph, node, visiting, visited):
+      return True
+
+  return False
+
+def has_cycle(graph, current, visiting, visited):
+  if current in visited:
+    return False
+
+  if current in visiting:
+    return True
+
+  visiting.add(current)
+
+  for neighbor in graph[current]:
+    if has_cycle(graph, neighbor, visiting, visited):
+      return True
+
+  visiting.remove(current)
+  visited.add(current)
+  return False
+
+
