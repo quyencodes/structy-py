3f5d4a59e95a62b764644ee380e257266369c328
Quyen Hoang <qduhoang@gmail.com>
2023-06-28 17:02:58 -0700

    tolerantTeams complete

--- 8-mixed_recall/tolerant_teams.py
+++ 8-mixed_recall/tolerant_teams.py
@@ -1,24 +1,31 @@
+"""
+get_teams helper function
+
+"""
+
 def tolerant_teams(rivalries):
-  graph = build_graph(rivalries)
-  coloring = {}
-  for node in graph:
-    if node not in coloring and not pick_teams(graph, node, coloring, False):
-      return False
-  return True
-
-def pick_teams(graph, node, coloring, current_color):
-  if node in coloring:
-    return coloring[node] == current_color
-
-  coloring[node] = current_color
-
-  for neighbor in graph[node]:
-    if not pick_teams(graph, neighbor, coloring, not current_color):
+  graph = get_teams(rivalries)
+  teams = {}
+  team_code = True
+  for player in graph:
+    team_code = not team_code
+    if player not in teams and not assign_teams(graph, player, teams, team_code):
       return False
 
   return True
 
-def build_graph(edges):
+def assign_teams(graph, current, teams, team_code):
+  if current in teams:
+    return team_code == teams[current]
+
+  teams[current] = team_code
+  for player in graph[current]:
+    if assign_teams(graph, player, teams, not team_code) == False:
+      return False
+
+  return True
+
+def get_teams(edges):
   graph = {}
   for edge in edges:
     a, b = edge
@@ -26,14 +33,8 @@ def build_graph(edges):
       graph[a] = []
     if b not in graph:
       graph[b] = []
+
     graph[a].append(b)
     graph[b].append(a)
+
   return graph
-"""
-{
-  philip: ['seb']
-  seb: ['philip']
-
-}
-
-"""
