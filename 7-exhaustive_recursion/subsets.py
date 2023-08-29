d55e013c0b07294695a19f9d1c3cfe23fd7b30ca
Quyen Hoang <qduhoang@gmail.com>
2023-08-29 15:53:22 -0700

    subsets complete

--- 7-exhaustive_recursion/subsets.py
+++ 7-exhaustive_recursion/subsets.py
@@ -1,18 +1,22 @@
+"""
+Take/Don't Take Algorithm. Base case: return a 2d matrix when i == len(elements). Use an index variable for better time complexity. Isolate elements[i]. Recursive call for i+1. Add first to the without_first list and return them together.
+"""
+
+
 def subsets(elements):
   return _subsets(elements, 0)
 
+
 def _subsets(elements, i):
-  if len(elements) == i:
-    return [ [] ]
+  if i == len(elements):
+    return [[]]
 
-  first = elements[i] # 'a'
-  subsets_without = _subsets(elements, i+1) # [ ['b', 'c'], ['c'], ['b'], [] ]
+  first = elements[i]
+  without_first = _subsets(elements, i + 1)  # [[], ['b'], ['c'], ['b', 'c']]
 
-  subsets_with = []
-  for subset in subsets_without:
-    subsets_with.append([first, *subset])
+  with_first = []
+  for sublist in without_first:
+    temp = [first] + sublist
+    with_first.append(temp)
 
-  return subsets_with + subsets_without
-
-
-
+  return with_first + without_first
