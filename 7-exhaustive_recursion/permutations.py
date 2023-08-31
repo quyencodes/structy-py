e37b10002266f04dce28a493fd4226dceed1a6e9
Quyen Hoang <qduhoang@gmail.com>
2023-08-30 17:31:54 -0700

    permutations complete

--- 7-exhaustive_recursion/permutations.py
+++ 7-exhaustive_recursion/permutations.py
@@ -1,17 +1,3 @@
-def permutations(items):
-  return _permutations(items, 0)
-
-def _permutations(items, i):
-  if len(items) == i:
-    return [ [] ]
-
-  first = items[i] # 'a'
-  perms_without_first = _permutations(items, i+1) # [['b', 'c'], ['c', 'b']]
-
-  full_permutations = []
-  for perm in perms_without_first:
-    for k in range(len(perm) + 1):
-      temp = perm[:k] + [first] + perm[k:]
-      full_permutations.append(temp)
-
-  return full_permutations
+"""
+Base case: Empty items, return [[]]. Isolate the current item. Iterate through the sublists. For each sublist, iterate through the len(sublist) + 1 and at each position insert self.
+"""
