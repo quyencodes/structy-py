4d9234429ebc88ce7e2673bac8b281a2aa07e8c6
Quyen Hoang <qduhoang@gmail.com>
2023-09-02 20:13:09 -0700

    parenthetical possibilities complete

--- 7-exhaustive_recursion/parenthetical_possibilies.py
+++ 7-exhaustive_recursion/parenthetical_possibilies.py
@@ -1,23 +1,34 @@
+"""
+base case:
+if len(s) == 0: return ['']
+
+recursive case:
+
+Base case: If len(s) == 0: return [""]. If no open bracket, iterate through recursive call and add self to the beginning of each result in the list. If there is an open bracket, get the substring from open to closing. Iterate through the group and add it to the recursive call.
+
+If we don't see an open bracket '(', iterate through the recursive call and add self to the beginning of each subresult. If we do see an open bracket,
+
+"""
+
+
 def parenthetical_possibilities(s):
   if len(s) == 0:
     return ['']
 
-  choices, remainder = get_choices(s)
-
+  first = s[0]
   all_possibilities = []
-  for choice in choices:
-    sub_possibilities = parenthetical_possibilities(remainder)
-    for possibility in sub_possibilities:
-      all_possibilities.append(choice + possibility)
+
+  if first.startswith('('):  # if we have a group
+    closing = s.index(')')
+    group = s[1:closing]
+    # print(group)
+    for letter in group:
+      sub_possibilities = parenthetical_possibilities(s[closing + 1:])
+      for sub in sub_possibilities:
+        all_possibilities.append(letter + sub)
+  else:  # if we don't have a group, just letter
+    possibilities = parenthetical_possibilities(s[1:])
+    for subresult in possibilities:
+      all_possibilities.append(first + subresult)
 
   return all_possibilities
-
-def get_choices(s):
-  first = s[0]
-  if s[0] == '(':
-    end = s.index(')')
-    group = s[1:end]
-    remaining = s[end+1:]
-    return (group, remaining)
-  else:
-    return (s[0], s[1:])
