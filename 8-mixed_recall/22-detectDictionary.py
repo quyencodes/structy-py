12e8415a1425498c5be7d086c799bf463b717f92
Quyen Hoang <qduhoang@gmail.com>
2023-09-10 19:04:03 -0700

    detectDictionary complete

--- 8-mixed_recall/22-detectDictionary.py
+++ 8-mixed_recall/22-detectDictionary.py
@@ -1,47 +1,38 @@
+"""
+Check two words at a time. Have a helper function determine with inputs word_1 and word_2 to see if these 2 words are ordered properly. Main function iterates through the dictionary and passes two words at a time to the main function.
+"""
+
+
 def detect_dictionary(dictionary, alphabet):
-  for i in range(0, len(dictionary) - 1):
-    word_1, word_2 = dictionary[i], dictionary[i + 1]
+  for i in range(len(dictionary) - 1):
+    j = i + 1
+    word_1, word_2 = dictionary[i], dictionary[j]
+
     if not lexical_order(alphabet, word_1, word_2):
       return False
 
   return True
 
+
 def lexical_order(alphabet, word_1, word_2):
-  alphabet_set = {letter: index for index, letter in enumerate(alphabet)}
+  alphaMap = {letter: index for index, letter in enumerate(alphabet)}
 
-  max_length = max(len(word_1), len(word_2))
+  i, j = 0, 0
 
-  for i in range(0, max_length):
+  while i < len(word_1) or j < len(word_2):
     if i == len(word_1):
       return True
-    if i == len(word_2):
+    if j == len(word_2):
       return False
 
-    letter_1, letter_2 = alphabet_set[word_1[i]], alphabet_set[word_2[i]]
-    if letter_1 < letter_2:
+    letter_1, letter_2 = word_1[i], word_2[j]
+
+    if alphaMap[letter_1] < alphaMap[letter_2]:
       return True
-    elif letter_1 > letter_2:
+    elif alphaMap[letter_1] > alphaMap[letter_2]:
       return False
 
-  return True
-
-def detect_dictionary(dictionary, alphabet):
-  for i in range(0, len(dictionary) - 1):
-    j = i + 1
-    if not lexical_order(dictionary[i], dictionary[j], alphabet):
-      return False
-
-  return True
-
-def lexical_order(word_1, word_2, alphabet):
-  max_length = max(len(word_1), len(word_2))
-
-  for i in range(0, max_length):
-    val_1 = alphabet.index(word_1[i]) if i < len(word_1) else float('-inf')
-    val_2 = alphabet.index(word_2[i]) if i < len(word_2) else float('-inf')
-    if val_1 < val_2:
-      return True
-    elif val_2 < val_1:
-      return False
+    i += 1
+    j += 1
 
   return True
